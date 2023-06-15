05c7f476e71f46a27f9c80a44604a9637bd4e25e
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-06-15 15:10:41 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main  - print a string "Programming is like building a multilingual puzzle
+ *
+ * Return: the outputs of puts function
+ */
+int main(void)
+{
+	char *s = ""Programming is like building a multilingual puzzle";
+	puts(s);
+
+}
