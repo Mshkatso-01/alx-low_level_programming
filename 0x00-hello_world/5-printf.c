95af3a0f80ea66410b0b51bcd21bd9e704f14578
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-06-15 16:26:12 -0700

    Preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
-/* 
+/*
+ * main -  C program that prints using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	reurn(0);
+}
