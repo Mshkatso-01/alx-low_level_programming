a5b1089f7634b9bcdd79d155f25ed4174a3ba05f
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-06-16 20:00:29 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all possible combinations of single-digit numbers
+ *
+ * Reasulte: resulte is 0 (success)
+ */
+int main(void)
+{
+	int a, b, c;
+	for (a = 1; a < 3; a++)
+		putchar(a);
+		for (b = 1; b < 3; b++)
+			putchar(b);
+			for (c = 1; b < 3; c++)
+				putchar(c);
+	putchar('\n')
+	return (0);
+}
+ 				
+
