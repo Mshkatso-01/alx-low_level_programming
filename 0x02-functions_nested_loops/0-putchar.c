296778ee63492acd54360ce6f1d171e01449a720
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-06-21 12:31:31 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
