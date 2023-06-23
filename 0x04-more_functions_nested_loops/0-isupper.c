5d253a53d19a3de7ad93ce10932889620e184c9e
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-06-23 05:44:49 -0700

    isuppere

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - a function that checks for uppercase character.
+ * Return - Always return 0 (success)
+ * @c:parameter had an int date type
+ * Return:function return 0 or 1
+ */
+int _isupper(int c)
+{
+
+	for (c = 65; c <= 90; c++)
+	{
+		return (1);
+	}
+
+
+
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+int mul(int a, int b);
+int main(void)
+{
+	int c = 4; d = 7; multi;
+
+	multi = mul(a * b)
+}
+int mul(int, int)
+{
+	return (a * b);
+	
