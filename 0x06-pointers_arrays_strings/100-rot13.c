45a9d74e2058addd8a3fc3ac7672aeb901987cda
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-07-04 10:37:38 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 - encodes string using rot13
+ * @string: pointer to string params
+ * Return: pointer to string
+ */
+char *rot13(char *string)
+{
+	int i = 0;
+	int j = 0;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotated[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (string[i])
+	{
+		while (data[j])
+		{
+			if (string[i] == data[j])
+			{
+				string[i] = rotated[j];
+				break;
+			}
+			j += 1;
+		}
+		j = 0;
+		i += 1;
+	}
+	return (string);
+}
