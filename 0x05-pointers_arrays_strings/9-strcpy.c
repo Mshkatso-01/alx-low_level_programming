9782d93e0cdc14754891d3ebd217ba69d2e3df2d
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-07-04 07:26:46 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strcpy - copy string to another buffer
+ *@dest: destination string
+ *@src: source string
+ *Return: char* - dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index += 1;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
