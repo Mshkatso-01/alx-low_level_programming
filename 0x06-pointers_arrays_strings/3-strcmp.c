31e854c2d19a409d1d98099f6d796022ca29b543
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-07-04 10:11:32 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strcmp - compares two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: int - difference of characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int result;
+
+	while ((s1[i] == s2[i]) && (s1[i] || s2[i]))
+	{
+		i += 1;
+	}
+	result = s1[i] - s2[i];
+	return (result);
+}
