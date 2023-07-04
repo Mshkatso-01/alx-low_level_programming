9fcf86ad2089478755479e7063ff28bf038ee53a
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-07-04 10:01:29 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strncat - concatenate up to a limit
+ *@dest: destination string
+ *@src: source string
+ *@n: limit
+ *Return: char* - destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *result;
+
+	result = dest;
+	while (*dest)
+	{
+		dest += 1;
+	}
+	while (src[i] && (i < n))
+	{
+		dest[i] = src[i];
+		i += 1;
+	}
+	dest[i] = '\0';
+	return (result);
+}
