909c3a791cef837e7885df51aa64123527e59ac8
mshkatso.sul0 <mshkatso.sul0@gmail.com>
2023-07-04 07:12:34 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *puts2 - prints every other character of a string
+ *@string: pointer to the string
+ *Return: void
+ */
+void puts2(char *string)
+{
+	int index = 0;
+
+	while (string[index])
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(string[index]);
+		}
+		index += 1;
+	}
+	_putchar('\n');
+}
+
